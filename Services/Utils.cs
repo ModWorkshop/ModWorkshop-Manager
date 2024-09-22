5a5a7711695b37cf90fc24a92ce1dcad624bd0fc
Luffyyy <luffydafloffi@gmail.com>
2024-09-23 02:49:50 +0300

    Fixed move not overriding files

--- Services/Utils.cs
+++ Services/Utils.cs
@@ -44,7 +44,7 @@ namespace MWSManager.Services
             foreach (FileInfo file in dir.GetFiles())
             {
                 string targetFilePath = Path.Combine(destinationDir, file.Name);
-                file.CopyTo(targetFilePath);
+                file.CopyTo(targetFilePath, true);
             }
 
             // If recursive and copying subdirectories, recursively call this method
